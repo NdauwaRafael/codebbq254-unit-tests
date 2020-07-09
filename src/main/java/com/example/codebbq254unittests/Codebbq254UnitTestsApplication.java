2686694ba9ddb7d1c2924bc5c07969ddc2078ca8
Raphael <raphaelndauwa@gmail.com>
2020-07-09 19:38:37 +0300

    add initial files

---
+++ src/main/java/com/example/codebbq254unittests/Codebbq254UnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.codebbq254unittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codebbq254UnitTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Codebbq254UnitTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/codebbq254unittests/Codebbq254UnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codebbq254unittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codebbq254UnitTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
