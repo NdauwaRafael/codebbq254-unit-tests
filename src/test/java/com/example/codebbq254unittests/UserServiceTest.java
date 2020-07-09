87fd9d4956362603deb69ef49c940d6d4cd34cae
Raphael <raphaelndauwa@gmail.com>
2020-07-09 20:24:08 +0300

    finalizr tests

--- src/test/java/com/example/codebbq254unittests/UserServiceTest.java
+++ src/test/java/com/example/codebbq254unittests/UserServiceTest.java
@@ -2,7 +2,7 @@ package com.example.codebbq254unittests;
 
 import com.example.codebbq254unittests.service.UserService;
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
